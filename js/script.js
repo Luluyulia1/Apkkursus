fca46ad8228fca7df33ce3a2c3bd6721b15260a0
Luluyulia1 <134262803+Luluyulia1@users.noreply.github.com>
2024-01-19 09:09:23 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var tombolMenu = document.getElementsByClassName('tombol-Home')[0];
+var menu = document.getElementsByClassName('Home')[0];
+
+tombolMenu.onclick = function() {
+    menu.classList.toggle('active');
+}
+
+menu.onclick = function() {
+    menu.classList.toggle('active');
+}
